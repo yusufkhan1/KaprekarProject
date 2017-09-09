aca0aa9e2ffde973d2ac5ce35d8920cb97238e7c
yusufkhan1 <31706103+yusufkhan1@users.noreply.github.com>
2017-09-08 21:02:31 -0500

    KaprekarUtils.java

--- src/unl/cse/kaprekar/KaprekarUtils.java
+++ src/unl/cse/kaprekar/KaprekarUtils.java
@@ -25,21 +25,23 @@ public class KaprekarUtils {
 	public static boolean isKaprekar(int n) {
 		
 		if(n < 1); {
-			return 0;
+			return false;
 		} 
 		
 		//we use a long to accommodate larger squares
-		long square = n * n;
-		int numDigits = (int) Math.log10(n) + 1;
+		long square = (long) java.lang.Math.pow(n,2);
+		int numDigits = (int) Math.log10(square) + 1;
 		long modulus = 1;
 		long first, second;
 		for(int i=1; i<=numDigits; i++) {
 			modulus *= 10;
-			frist = square / modlus;
+			first = square / modulus;
 			second = square % modulus;
 			if(first > 0 &&
-			   first + second = n) {
-				return true;
+			   first + second == n &&
+				second > 0 ||
+				n == 1) {
+					return true;
 			}
 		}
 		return false;
